783f25daf2a0abefe2d30a2a5d0f9c752acb7159
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-09 20:20:06 -0300

    Rename exer5.cpp to exer5.c

--- exer5.c
+++ exer5.c
@@ -1,6 +1,6 @@
 /*Fazer um algoritmo para auxiliar um construtor a descobrir a quantidade de rolos e a quantidade de 
 metros avulsos de fios que ele precisa adquirir. Sabe-se que o construtor tem a quantidade total 
-de fios em metros a serem utilizados na instalação elétrica da obra e que cada rolo de fio tem 50 metros.*/
+de fios em metros a serem utilizados na instalaÃ§Ã£o elÃ©trica da obra e que cada rolo de fio tem 50 metros.*/
 
 #include <stdio.h>
 
