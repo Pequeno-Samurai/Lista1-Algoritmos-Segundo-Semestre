a5a592358339ecf5a3a07c346b5953d3c74e0875
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-11 07:22:47 -0300

    Rename exer6.cpp to exer6.c

--- exer6.c
+++ exer6.c
@@ -1,6 +1,6 @@
-/*Leia 2 valores A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno,
-sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). 
-Assuma que cada nota pode ir de 0 até 10.0. */
+/*Leia 2 valores A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a mÃ©dia do aluno,
+sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto Ã© 11). 
+Assuma que cada nota pode ir de 0 atÃ© 10.0. */
 
 #include <stdio.h>
 
